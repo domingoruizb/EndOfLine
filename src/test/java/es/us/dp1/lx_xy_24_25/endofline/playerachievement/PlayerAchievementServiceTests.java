9aad500bc5f01acfbaebb5c4563224c6716cfe24
Angelo Moraschi <68395930+amoraschi@users.noreply.github.com>
2025-11-02 19:03:41 +0100

    PlayerAchievement tests

---
+++ src/test/java/es/us/dp1/lx_xy_24_25/endofline/playerachievement/PlayerAchievementServiceTests.java
@@ -0,0 +1,10 @@
+package es.us.dp1.lx_xy_24_25.endofline.playerachievement;
+
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@AutoConfigureTestDatabase
+public class PlayerAchievementServiceTests {
+
+}
