ce769b97e3a2d457a0b4130a9184736f45a363f9
Makar Lavrov <makarlavrov2005@gmail.com>
2025-11-05 14:53:56 +0100

    added a setter for GamePlayer, so Hibernate won't through a mistake
    important: mutate the existing collection instance (clear and add) instead of replacing the reference, so Hibernate can track orphan removals properly

--- src/main/java/es/us/dp1/lx_xy_24_25/endofline/user/User.java
+++ src/main/java/es/us/dp1/lx_xy_24_25/endofline/user/User.java
@@ -3,6 +3,7 @@ package es.us.dp1.lx_xy_24_25.endofline.user;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
+import java.util.ArrayList;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import es.us.dp1.lx_xy_24_25.endofline.gameplayer.GamePlayer;
@@ -18,44 +19,67 @@ import lombok.Setter;
 @Table(name = "users")
 public class User extends NamedEntity {
 
-	@NotNull
-	String surname;
+    @NotNull
+    String surname;
 
-	@NotNull
-	LocalDate birthdate;
+    @NotNull
+    LocalDate birthdate;
 
-	@NotNull
-	String email;
+    @NotNull
+    String email;
 
-	@NotNull
-	@Column(unique = true)
-	String username;
+    @NotNull
+    @Column(unique = true)
+    String username;
 
-	@NotNull
-	String password;
+    @NotNull
+    String password;
 
-	@NotNull
-	@ManyToOne(optional = false)
-	@JoinColumn(name = "authority")
-	Authorities authority;
+    @NotNull
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "authority")
+    Authorities authority;
 
-	String avatar;
+    String avatar;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
     @JsonIgnore
-    private List<GamePlayer> gamePlayer;
+    private List<GamePlayer> gamePlayer = new ArrayList<>();
 
-	public Boolean hasAuthority(String auth) {
-		return authority.getAuthority().equals(auth);
-	}
+    public Boolean hasAuthority(String auth) {
+        return authority.getAuthority().equals(auth);
+    }
 
-	public Boolean hasAnyAuthority(String... authorities) {
-		Boolean cond = false;
-		for (String auth : authorities) {
-			if (auth.equals(authority.getAuthority()))
-				cond = true;
-		}
-		return cond;
-	}
+    public Boolean hasAnyAuthority(String... authorities) {
+        Boolean cond = false;
+        for (String auth : authorities) {
+            if (auth.equals(authority.getAuthority()))
+                cond = true;
+        }
+        return cond;
+    }
+
+
+	// setter for the collection used by Hibernate with orphanRemoval = true
+	// it is important to mutate the existing collection instance (clear and add)
+	// instead of replacing the reference, so Hibernate can track orphan removals properly
+
+	public void setGamePlayer(List<GamePlayer> gamePlayers) {
+        if (this.gamePlayer == null) {
+            this.gamePlayer = new ArrayList<>();
+        } else {
+            this.gamePlayer.clear();
+        }
+
+        if (gamePlayers != null) {
+            for (GamePlayer gp : gamePlayers) {
+                if (gp != null) {
+                    gp.setUser(this);
+                }
+            }
+            this.gamePlayer.addAll(gamePlayers);
+        }
+    }
 
 }
+
