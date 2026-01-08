3289b06e4a5eb342d710e4f7902579961b721037
DOMINGO RUIZ BELLIDO <domruibel@alum.us.es>
2026-01-08 20:24:22 +0100

    Loading screen styles modified

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -14,7 +14,7 @@ export default function PrivateRoute ({
             if (jwt == null) {
                 return
             }
-
+            await new Promise(res => setTimeout(res, 2000));
             await getData(
                 `/api/v1/auth/validate?token=${jwt}`
             )
@@ -24,8 +24,9 @@ export default function PrivateRoute ({
     }, [])
 
     return loading ? (
-        <div>
-            Loading...
+        <div className="error-page-container">
+            <h1 className="error-title">Loading</h1>
+            <h2 className="error-subtitle">Please wait</h2>
         </div>
     ) : (
         data ? (
