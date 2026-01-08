39205cb44de22360ba8a73b242c7a25a8f490ef7
DOMINGO RUIZ BELLIDO <domruibel@alum.us.es>
2026-01-08 20:25:32 +0100

    Delay removed

--- frontend/src/components/PrivateRoute.js
+++ frontend/src/components/PrivateRoute.js
@@ -14,7 +14,6 @@ export default function PrivateRoute ({
             if (jwt == null) {
                 return
             }
-            await new Promise(res => setTimeout(res, 2000));
             await getData(
                 `/api/v1/auth/validate?token=${jwt}`
             )
@@ -25,7 +24,7 @@ export default function PrivateRoute ({
 
     return loading ? (
         <div className="error-page-container">
-            <h1 className="error-title">Loading</h1>
+            <h1 className="error-title">Loading...</h1>
             <h2 className="error-subtitle">Please wait</h2>
         </div>
     ) : (
