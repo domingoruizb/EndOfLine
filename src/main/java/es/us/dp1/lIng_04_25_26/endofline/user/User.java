3d18a52ffa4e42e2008a301ab062f875c8393ef4
DOMINGO RUIZ BELLIDO <domruibel@alum.us.es>
2026-01-14 11:04:02 +0100

    Applied Builder pattern on User

--- src/main/java/es/us/dp1/lIng_04_25_26/endofline/user/User.java
+++ src/main/java/es/us/dp1/lIng_04_25_26/endofline/user/User.java
@@ -48,21 +48,82 @@ public class User extends NamedEntity {
     @JsonIgnore
     private List<GamePlayer> gamePlayers = new ArrayList<>();
 
-    public static User build (
-        SignupRequest request,
-        String password,
-        Authority authority
-    ) {
-        User user = new User();
-        user.setUsername(request.getUsername());
-        user.setPassword(password);
-        user.setAuthority(authority);
-        user.setAvatar(request.getAvatar());
-        user.setEmail(request.getEmail());
-        user.setBirthdate(LocalDate.parse(request.getBirthdate()));
-        user.setName(request.getName());
-        user.setSurname(request.getSurname());
-        return user;
+
+    public static class UserBuilder {
+        private String surname;
+        private LocalDate birthdate;
+        private String email;
+        private String username;
+        private String password;
+        private Authority authority;
+        private String avatar;
+        private String name;
+
+        public UserBuilder surname(String surname) {
+            this.surname = surname;
+            return this;
+        }
+
+        public UserBuilder birthdate(LocalDate birthdate) {
+            this.birthdate = birthdate;
+            return this;
+        }
+
+        public UserBuilder email(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public UserBuilder username(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public UserBuilder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public UserBuilder authority(Authority authority) {
+            this.authority = authority;
+            return this;
+        }
+
+        public UserBuilder avatar(String avatar) {
+            this.avatar = avatar;
+            return this;
+        }
+
+        public UserBuilder name(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public User build() {
+            User user = new User();
+            user.setSurname(this.surname);
+            user.setBirthdate(this.birthdate);
+            user.setEmail(this.email);
+            user.setUsername(this.username);
+            user.setPassword(this.password);
+            user.setAuthority(this.authority);
+            user.setAvatar(this.avatar);
+            user.setName(this.name);
+            return user;
+        }
+    }
+
+    public static User build(SignupRequest request, String password, Authority authority) {
+        return new UserBuilder()
+                .username(request.getUsername())
+                .password(password)
+                .authority(authority)
+                .avatar(request.getAvatar())
+                .email(request.getEmail())
+                .birthdate(LocalDate.parse(request.getBirthdate()))
+                .name(request.getName())
+                .surname(request.getSurname())
+                .build();
     }
 
 }
